746db35e94ddfa63b182ab53f53a847c4329776f
«Maria <284004@uis.com>
2026-09-25 11:25:14 +0200

    lastet ned rikitg csv fil

--- ovinger/oving6/kode.py
+++ ovinger/oving6/kode.py
@@ -1,11 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import requests
+import pandas as pd
 
+# Oppgave 1
 t = np.linspace(0, 24, 400)
 
 A = 800
 mu = 13
-sigma = 2
+sigma = 3
 
 G = A * np.exp(-(t-mu)**2/(2*sigma**2))
 
@@ -14,5 +17,10 @@ plt.title("Gauss-modell")
 plt.xlabel("Time")
 plt.ylabel("Innstråling")
 plt.grid()
-#plt.show()
+plt.show()
 #plt.savefig("ovinger/oving6/gauss_modell.png")
+
+# Oppgave 5
+df = pd.read_csv("ovinger/oving6/innstraling_monaco.csv")
+
+print(df.head())
